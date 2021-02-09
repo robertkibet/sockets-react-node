a9948804df3f979d80dbfa1ced052403f8863328
Robert Kibet <kipronokrobert@gmail.com>
2021-02-09 14:28:39 +0300

    clear cors

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const cors = require('cors')
 
-router.get("/", cors(), (req, res) => {
+router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
 });
 
